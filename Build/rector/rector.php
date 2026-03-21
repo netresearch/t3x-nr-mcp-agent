de95e92c6b61fd28cbbf0b80f409ec070e74c382
Tobias Hein <tobias.hein@netresearch.de>
2026-03-21 18:51:46 +0100

    refactor: reorganize quality configs into Build/ subdirectories

--- Build/rector/rector.php
+++ Build/rector/rector.php
@@ -8,14 +8,14 @@ use Rector\DeadCode\Rector\Property\RemoveUnusedPrivatePropertyRector;
 
 return RectorConfig::configure()
     ->withPaths([
-        __DIR__ . '/Classes',
+        __DIR__ . '/../../Classes',
     ])
     ->withPhpSets(php82: true)
     ->withPreparedSets(codeQuality: true, deadCode: true)
     ->withSkip([
         // crdate hydrated from DB, kept for completeness
         RemoveUnusedPrivatePropertyRector::class => [
-            __DIR__ . '/Classes/Domain/Model/Conversation.php',
+            __DIR__ . '/../../Classes/Domain/Model/Conversation.php',
         ],
         // Verbose instanceof checks not preferred over null checks
         FlipTypeControlToUseExclusiveTypeRector::class,
