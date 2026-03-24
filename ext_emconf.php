16c001a75caaadf018ace80a0cf197873ddc8434
Tobias Hein <tobias.hein@netresearch.de>
2026-03-24 16:41:54 +0100

    chore: add release/TER workflows and fix ext_emconf constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,9 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'Netresearch DTT GmbH',
     'constraints' => [
         'depends' => [
+            'php' => '8.2.0-8.99.99',
             'typo3' => '13.4.0-14.99.99',
+            'nr_llm' => '0.6.0-0.99.99',
         ],
     ],
 ];
